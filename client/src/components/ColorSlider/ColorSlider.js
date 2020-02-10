7af8a9fb6a27ebdbc21a354b5e50b6b589ae1b87
Anthony Shew <anthonyshew@gmail.com>
2020-02-10 13:33:13 -0700

    Production colors from Matt.

--- client/src/components/ColorSlider/ColorSlider.js
+++ client/src/components/ColorSlider/ColorSlider.js
@@ -8,36 +8,112 @@ const ColorSlider = ({ colorType, ...props }) => {
     const [, dispatch] = useStateValue()
 
     const colorOptions = [{
+        colorName: "Black",
+        hexCode: "#000000",
+    },
+    {
+        colorName: "White",
+        hexCode: "#ffffff",
+    },
+    {
+        colorName: "Cool Gray",
+        hexCode: "#969799",
+    },
+    {
+        colorName: "Charcoal",
+        hexCode: "#56555b",
+    },
+    {
+        colorName: "Maroon",
+        hexCode: "#960001",
+    },
+    {
         colorName: "Red",
-        hexCode: "red",
+        hexCode: "#fe0000",
     },
     {
-        colorName: "orange",
-        hexCode: "orange",
+        colorName: "Deep Orange",
+        hexCode: "#ff5517",
     },
     {
-        colorName: "yellow",
-        hexCode: "yellow",
+        colorName: "Bright Orange",
+        hexCode: "#ff8500",
     },
     {
-        colorName: "green",
-        hexCode: "green",
+        colorName: "Light Orange",
+        hexCode: "#ffcf9e",
     },
     {
-        colorName: "blue",
-        hexCode: "blue",
+        colorName: "Gold",
+        hexCode: "#c99600",
     },
     {
-        colorName: "purple",
-        hexCode: "purple",
+        colorName: "Vegas Gold",
+        hexCode: "#c0b460",
     },
     {
-        colorName: "white",
-        hexCode: "white",
+        colorName: "Gold Yellow",
+        hexCode: "#ffe001",
     },
     {
-        colorName: "black",
-        hexCode: "black",
+        colorName: "Yellow",
+        hexCode: "#ffff01",
+    },
+    {
+        colorName: "Neon Green",
+        hexCode: "#76bc1e",
+    },
+    {
+        colorName: "Green",
+        hexCode: "#008100",
+    },
+    {
+        colorName: "Hunter Green",
+        hexCode: "#195533",
+    },
+    {
+        colorName: "Teal",
+        hexCode: "#008081",
+    },
+    {
+        colorName: "Dark Navy",
+        hexCode: "#001f4e",
+    },
+    {
+        colorName: "Navy",
+        hexCode: "#10284c",
+    },
+    {
+        colorName: "Rampage Blue",
+        hexCode: "#1d4e90",
+    },
+    {
+        colorName: "Blue",
+        hexCode: "#0000fe",
+    },
+    {
+        colorName: "Royal Blue",
+        hexCode: "#1d68e5",
+    },
+    {
+        colorName: "Carolina Blue",
+        hexCode: "#8abade",
+    },
+    {
+        colorName: "Columbia Blue",
+        hexCode: "#75defe",
+    },
+    {
+        colorName: "Purple",
+        hexCode: "#960085",
+    },
+    {
+        colorName: "Dark Purple",
+        hexCode: "#4d225a",
+    },
+    {
+        colorName: "Pink",
+        hexCode: "#fe187b",
     }]
 
     return (
