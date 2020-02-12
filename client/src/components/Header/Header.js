a93a1dd206f8614440dc7abba5e0f66b9a3206de
Anthony Shew <anthonyshew@gmail.com>
2020-02-11 18:58:49 -0700

    First rough draft to show Matt.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './_Header.scss'
 
+import SVG from '../SVG'
+
 const Header = ({ ...props }) => {
     return (
         <div className="header">
-            Make something great with the Header component here!
-    </div>
+            <h1 className="h1">Flatbill Customizer</h1>
+            <SVG name="platypus" className="platypus" />
+        </div>
     )
 }
 
