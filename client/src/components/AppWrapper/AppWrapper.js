08435d6c897db851542393d22aa5548554b7ed57
Anthony Shew <anthonyshew@gmail.com>
2020-02-24 09:50:06 -0700

    Prevent zooming?

--- client/src/components/AppWrapper/AppWrapper.js
+++ client/src/components/AppWrapper/AppWrapper.js
@@ -7,6 +7,18 @@ const AppWrapper = ({ children }) => {
 
     useEffect(() => {
         window.addEventListener("resize", () => setViewportHeight(window.innerHeight))
+        document.addEventListener('touchmove', (event) => {
+            if (event.scale !== 1) { event.preventDefault() }
+        }, { passive: false })
+
+        let lastTouchEnd = 0;
+        document.addEventListener('touchend', (event) => {
+            let now = (new Date()).getTime();
+            if (now - lastTouchEnd <= 300) {
+                event.preventDefault();
+            }
+            lastTouchEnd = now;
+        }, { passive: false });
     }, [])
 
     return (
