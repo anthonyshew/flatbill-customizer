595a185e90b40d6b3b26bae02e979b2a5afdb1a5
Anthony Shew <anthonyshew@gmail.com>
2020-03-16 18:50:27 -0700

    Fully fleshed checkout.

--- client/src/components/checkout/Checkout.js
+++ client/src/components/checkout/Checkout.js
@@ -7,6 +7,7 @@ import {
     useStripe,
     useElements,
 } from '@stripe/react-stripe-js'
+import { useForm } from 'react-hook-form'
 import useStateValue from '../../lib/hooks/useStateValue'
 
 import SVG from '../SVG/SVG'
@@ -25,80 +26,18 @@ const style = {
 }
 
 const CheckoutForm = () => {
+    const { register, handleSubmit, errors } = useForm()
     const stripe = useStripe()
     const elements = useElements()
     const [{ price, teamDetails }, dispatch] = useStateValue()
 
-    const [billingDetails, setBillingDetails] = useState({
-        address: {
-            city: '',
-            country: '',
-            line1: '',
-            line2: '',
-            postal_code: '',
-            state: '',
-        },
-        email: '',
-        name: '',
-        phone: '',
-    })
-
-    const [shippingDetails, setShippingDetails] = useState({
-        address: {
-            city: '',
-            country: '',
-            line1: '',
-            line2: '',
-            postal_code: '',
-            state: '',
-        },
-        email: '',
-        name: '',
-        phone: '',
-    })
-
     const [matchDetails, setMatchDetails] = useState(true)
+    const [stripeError, setStripeError] = useState({
+        bool: false,
+        message: ""
+    })
 
-
-    const handleBasicShippingChange = (e) => {
-        setShippingDetails({
-            ...shippingDetails,
-            [e.target.id]: e.target.value
-        })
-    }
-
-    const handleShippingChange = (e) => {
-        setShippingDetails({
-            ...shippingDetails,
-            address: {
-                ...shippingDetails.address,
-                [e.target.id]: e.target.value
-            }
-        })
-    }
-
-    const handleBasicBillingChange = (e) => {
-        setBillingDetails({
-            ...billingDetails,
-            [e.target.id]: e.target.value
-        })
-    }
-
-    const handleBillingChange = (e) => {
-        setBillingDetails({
-            ...billingDetails,
-            address: {
-                ...billingDetails.address,
-                [e.target.id]: e.target.value
-            }
-        })
-    }
-
-    const handleCheckboxChange = (e) => {
-        setMatchDetails(e.target.checked)
-    }
-
-    const handleSubmit = async (event) => {
+    const onSubmit = async (formData, event) => {
         event.preventDefault()
 
         fetch('/checkout', {
@@ -110,25 +49,32 @@ const CheckoutForm = () => {
         })
             .then(res => res.json())
             .then(async (res) => {
-
-                console.log({
-                    card: elements.getElement(CardNumberElement),
-                    billing_details: billingDetails
-                })
-
-                const { error, paymentMethod } = await stripe.confirmCardPayment(`${res.client_secret}`, {
+                const { error } = await stripe.confirmCardPayment(`${res.client_secret}`, {
                     payment_method: {
                         card: elements.getElement(CardNumberElement),
-                        billing_details: matchDetails ? shippingDetails : billingDetails
+                        billing_details: {
+                            address: {
+                                city: formData.bill_city ?? formData.ship_city,
+                                country: formData.bill_country ?? formData.ship_country,
+                                line1: formData.bill_line1 ?? formData.ship_line1,
+                                line2: formData.bill_line2 ?? formData.ship_line2 ?? "",
+                                postal_code: formData.bill_postal_code ?? formData.ship_postal_code,
+                                state: formData.bill_state ?? formData.ship_state,
+                            }
+                        }
                     }
                 })
 
-                console.log(error, paymentMethod)
+                if (error) {
+                    setStripeError({ bool: true, message: error.message })
+                } else {
+                    dispatch({ type: "STEP_CHANGE", step: 5 })
+                }
             })
     }
 
     return (
-        <form className="checkout-form" onSubmit={handleSubmit}>
+        <form className="checkout-form" onSubmit={handleSubmit(onSubmit)}>
             <button
                 className="button-back"
                 onClick={() => dispatch({ type: "STEP_CHANGE", step: 3 })}
@@ -137,105 +83,107 @@ const CheckoutForm = () => {
                 </button>
             <h2>Checkout</h2>
             <h3>Get Your Jerseys</h3>
-            <h4>Basic Info</h4>
+            <h4>Shipping Info</h4>
             <label>
                 Name
                 <input
-                    id="name"
+                    ref={register({ required: true })}
+                    name="ship_name"
                     className="form-input"
-                    onChange={handleBasicShippingChange}
-                    value={shippingDetails.name}
                 />
             </label>
+            {errors.ship_name && <p className="error">Name Required</p>}
             <label>
                 E-Mail
                 <input
-                    id="email"
+                    ref={register({ required: true, pattern: /^.+@[^].*\.[a-z]{2,}$/ })}
+                    name="email"
                     className="form-input"
-                    onChange={handleBasicShippingChange}
-                    value={shippingDetails.email}
                 />
             </label>
+            {errors.email && <p className="error">Valid Email Required</p>}
             <label>
                 Phone
                 <input
-                    id="phone"
+                    ref={register({ required: true, minLength: 10, maxLength: 10 })}
+                    name="phone"
                     className="form-input"
-                    onChange={handleBasicShippingChange}
-                    value={shippingDetails.phone}
                 />
             </label>
-            <h4>Shipping Info</h4>
+            {errors.phone && <p className="error mb-1">Valid Phone Number Required</p>}
             <label>
                 Address Line 1
                 <input
-                    id="line1"
+                    ref={register({ required: true })}
+                    name="ship_line1"
                     className="form-input"
-                    onChange={handleShippingChange}
-                    value={shippingDetails.line1}
                 />
             </label>
+            {errors.ship_line1 && <p className="error mb-1">Address Required</p>}
             <label>
                 Address Line 2 (Optional)
                 <input
-                    id="line2"
+                    ref={register}
+                    name="ship_line2"
                     className="form-input"
-                    onChange={handleShippingChange}
-                    value={shippingDetails.line2}
                 />
             </label>
             <label>
                 City
                 <input
-                    id="city"
+                    ref={register({ required: true })}
+                    name="ship_city"
                     className="form-input"
-                    onChange={handleShippingChange}
-                    value={shippingDetails.city}
                 />
             </label>
+            {errors.ship_city && <p className="error">City Required</p>}
             <label>
                 State
                 <input
-                    id="state"
+                    ref={register({ required: true })}
+                    name="ship_state"
                     className="form-input"
-                    onChange={handleShippingChange}
-                    value={shippingDetails.state}
                 />
             </label>
+            {errors.ship_state && <p className="error">State Required</p>}
             <label>
                 Postal Code
                 <input
-                    id="postal_code"
+                    ref={register({ required: true, minLength: 5, maxLength: 5 })}
+                    name="ship_postal_code"
                     className="form-input"
-                    onChange={handleShippingChange}
-                    value={shippingDetails.postal_code}
                 />
             </label>
+            {errors.ship_postal_code && <p className="error">Postal Code Required</p>}
             <label>
                 Country
-                <input
-                    id="country"
+                <select
+                    ref={register({ required: true })}
+                    name="ship_country"
                     className="form-input"
-                    onChange={handleShippingChange}
-                    value={shippingDetails.country}
-                />
+                >
+                    <option value="US">United States</option>
+                    <option value="CA">Canada</option>
+                </select>
             </label>
+            {errors.ship_country && <p className="error mb-1">Country Required</p>}
             <h4>Billing Info</h4>
             <div className="container-checkbox">
                 <label className="checkbox">
                     <input
                         type="checkbox"
                         checked={matchDetails}
-                        onChange={handleCheckboxChange}
+                        onChange={() => setMatchDetails(!matchDetails)}
                     />
                     <span></span>
                 </label>
                 <p>My billing info is the same as my shipping info.</p>
             </div>
-            {!matchDetails && <BillingDetailsInputs
-                handleBasicBillingChange={handleBasicBillingChange}
-                handleBillingChange={handleBillingChange}
-                billingDetails={billingDetails} />}
+            <BillingDetails
+                matchDetails={matchDetails}
+                register={register}
+                errors={errors}
+            />
             <h4>Order Summary</h4>
             <div className="container-order-summary">
                 <p>Custom Baseball Jersey x {teamDetails.length}</p>
@@ -264,7 +212,8 @@ const CheckoutForm = () => {
                     options={style}
                 />
             </label>
-            <button type="submit" disabled={!stripe}>
+            {stripeError.bool && <p className="error mb-1">{stripeError.message}</p>}
+            <button type="submit" className="submit" disabled={!stripe}>
                 Pay
       </button>
         </form>
@@ -273,75 +222,93 @@ const CheckoutForm = () => {
 
 export default CheckoutForm
 
-const BillingDetailsInputs = ({
-    handleBasicBillingChange,
-    handleBillingChange,
-    billingDetails }) => {
+const BillingDetails = ({ matchDetails, register, errors }) => {
+
     return (
-        <>
+        <div className="container-billing-details" style={matchDetails ? { display: "none" } : {}}>
             <label>
                 Name
                 <input
-                    id="name"
+                    ref={!matchDetails ? register({ required: true }) : null}
+                    name="bill_name"
                     className="form-input"
-                    onChange={handleBasicBillingChange}
-                    value={billingDetails.name}
                 />
             </label>
+            {errors.bill_name && <p className="error">Name Required</p>}
+            <label>
+                Email
+                <input
+                    ref={!matchDetails ? register({ required: true, pattern: /^.+@[^].*\.[a-z]{2,}$/ }) : null}
+                    name="bill_email"
+                    className="form-input"
+                />
+            </label>
+            {errors.bill_email && <p className="error mb-1">Valid Email Required</p>}
+            <label>
+                Phone Number
+                <input
+                    ref={!matchDetails ? register({ required: true, minLength: 10, maxLength: 10 }) : null}
+                    name="bill_phone"
+                    className="form-input"
+                />
+            </label>
+            {errors.bill_phone && <p className="error mb-1">Valid Phone Number Required</p>}
             <label>
                 Address Line 1
             <input
-                    id="line1"
+                    ref={!matchDetails ? register({ required: true }) : null}
+                    name="bill_line1"
                     className="form-input"
-                    onChange={handleBillingChange}
-                    value={billingDetails.line1}
                 />
             </label>
+            {errors.bill_line1 && <p className="error mb-1">Address Required</p>}
             <label>
                 Address Line 2 (Optional)
             <input
-                    id="line2"
+                    ref={!matchDetails ? register : null}
+                    name="bill_line2"
                     className="form-input"
-                    onChange={handleBillingChange}
-                    value={billingDetails.line2}
                 />
             </label>
             <label>
                 City
             <input
-                    id="city"
+                    ref={!matchDetails ? register({ required: true }) : null}
+                    name="bill_city"
                     className="form-input"
-                    onChange={handleBillingChange}
-                    value={billingDetails.city}
                 />
             </label>
+            {errors.bill_city && <p className="error">City Required</p>}
             <label>
                 State
             <input
-                    id="state"
+                    ref={!matchDetails ? register({ required: true }) : null}
+                    name="bill_state"
                     className="form-input"
-                    onChange={handleBillingChange}
-                    value={billingDetails.state}
                 />
             </label>
+            {errors.bill_state && <p className="error">State Required</p>}
             <label>
                 Postal Code
             <input
-                    id="postal_code"
+                    ref={!matchDetails ? register({ required: true, minLength: 5, maxLength: 5 }) : null}
+                    name="bill_postal_code"
                     className="form-input"
-                    onChange={handleBillingChange}
-                    value={billingDetails.postal_code}
                 />
             </label>
+            {errors.bill_postal_code && <p className="error">Postal Code Required</p>}
             <label>
                 Country
-            <input
-                    id="country"
+                <select
+                    ref={register({ required: true })}
+                    name="bill_country"
                     className="form-input"
-                    onChange={handleBillingChange}
-                    value={billingDetails.country}
-                />
+                >
+                    <option value="US">United States</option>
+                    <option value="CA">Canada</option>
+                </select>
             </label>
-        </>
+            {errors.bill_country && <p className="error mb-1">Country Required</p>}
+        </div>
     )
 }
